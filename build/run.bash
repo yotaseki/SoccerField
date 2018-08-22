d54dca220d444b43622c73f66dd38b77ab717700
ChisatoKasebayashi <chisato.rukia@gmail.com>
2018-08-22 18:41:13 +0900

    update run.bash

--- build/run.bash
+++ build/run.bash
@@ -4,9 +4,14 @@ mv output out_bak
 mkdir output
 cmake ..
 make -j
-mkdir images wearout labels
+mkdir images wearout labels_tmp
 mkdir images_blur wearout_blur labels_gray
 ./soccerField
 ./Blur
-mv images wearout labels output
+mv images wearout labels_tmp output
 mv images_blur wearout_blur labels_gray output
+
+# mv output ../appendColorMap/
+# cd ../appendColorMap
+# ./run.bash output/labels_gray
+# mv out output/labels
